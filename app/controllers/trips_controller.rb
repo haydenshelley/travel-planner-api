aad58d5aca69e4a99f9b39767da1002606645260
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-02 11:18:26 -0700

    trip edit route

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -20,6 +20,17 @@ class TripsController < ApplicationController
     render :show
   end
 
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    @trip.update(
+      title: params[:title] || @trip.title,
+      image_url: params[:image_url] || @trip.image_url,
+      start_time: params[:start_time] || @trip.start_time,
+      end_time: params[:end_time] || @trip.end_time
+    )
+    render :show
+  end
+
   def destroy
     @trip = Trip.find_by(id: params[:id])
     @trip.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "/trips" => "trips#index"
   post "/trips" => "trips#create"
   get "/trips/:id" => "trips#show"
+  patch "/trips/:id" => "trips#update"
   delete "/trips/:id" => "trips#destroy"
 
   #places routes
