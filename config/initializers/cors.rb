0ed2401a4d5ae55e1940d45b63e907eb847c7014
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-11 13:45:46 -0700

    redo cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "https://waypoints-hfeo.onrender.com"
+    origins "localhost:5173", "http://waypoints-hfeo.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
